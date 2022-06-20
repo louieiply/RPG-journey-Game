9b5368b4521fa95d92eef7ee944ff18f22b2d4ab
Zekun Chen <zekunchen97@gmail.com>
2022-06-20 18:40:41 +0930

    Routes v1

---
+++ controllers/api/index.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
 
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+
+router.use((req, res) => {
+    res.send("Wrong Route!")
+  });
+
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
