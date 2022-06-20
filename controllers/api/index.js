871f6b46b8e09bf05751419ed3993652cfb6b857
louieiply <louieiply@gmail.com>
2022-06-20 19:12:23 +0930

    Update Index route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const game = require('./Game');
+const comment = require('./Comment');
+const categories = require('./')
 
+router.use('/categories',categories);
+router.use('/comments',comment);
+router.use('/games',game);
+router.use('/users',userRoutes);
 
 module.exports = router;
