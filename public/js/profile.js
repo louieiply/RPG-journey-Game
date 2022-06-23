a0913b2e3a15b6987b27e05838312d29d12f3091
Peter Jolley <peterjolley456@gmail.com>
2022-06-23 19:38:50 +0930

    login profile and sign-up js files created

---
+++ public/js/profile.js
@@ -0,0 +1,45 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+    const name = document.querySelector('#game-name').value;
+    const description = document.querySelector('game-desc').value;
+    if (name && description) {
+        const response = await fetch('/api/post', {
+            method: 'POST',
+            body: JSON.stringify({
+                name,
+                description
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert('Failed to Create Game ');
+        }
+    }
+};
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to delete Game');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-post-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.game-list')
+    .addEventListener('click', delButtonHandler);
