bd476f200b9729703adf4f512b1e5255d4d45fc1
Zekun Chen <zekunchen97@gmail.com>
2022-06-23 19:43:42 +0930

    main page styling

--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,6 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
-<<<<<<< HEAD
-=======
-
->>>>>>> f17501ef00d8817d88bc30e075d9b43fc7a35693
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
