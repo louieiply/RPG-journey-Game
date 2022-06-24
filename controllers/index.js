9b4b3f870684734c3241c860ba129cb048685951
Peter Jolley <peterjolley456@gmail.com>
2022-06-24 23:40:07 +0930

    Profile now linked with story with valid game data

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,27 @@
 const router = require('express').Router();
 
+const { Game } = require('../models');
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+
+router.use('/story/:gameId', async (req, res) => {
+    console.log("hello" + req.params.gameId )
+    //x = JSON.parse(req)
+
+    game = (await Game.findOne({
+        where: {
+          id: req.params.gameId
+        }
+      })).toJSON()
+      console.log("game" + JSON.stringify(game))
+    //select * from game where game.id = req.params.gameId
+    res.render('story', { 
+        game
+  });
+})
 
 module.exports = router;
