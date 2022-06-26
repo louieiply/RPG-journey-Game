89f31e9b587e210cbe44f3b3ec53715655e19a0b
Peter Jolley <peterjolley456@gmail.com>
2022-06-26 20:57:09 +0930

    Signup created

--- public/js/login.js
+++ public/js/login.js
@@ -25,6 +25,38 @@ const loginFormHandler = async (event) => {
     }
 }
 
-document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler)
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#name-signup').value;
+    const email = document.querySelector('#email-signup').value;
+    const password = document.querySelector('#password-signup');
+  
+    if (name && email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ 
+            name, 
+            email, 
+            password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert(response.statusText);
+        alert('Sign-up has failed ');
+      }
+    }
+  };
+  
+  document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler)
+
+
+  document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
+
