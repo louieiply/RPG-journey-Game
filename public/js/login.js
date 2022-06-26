fc9d06f4c6fe41a1178250c5039a66c4c1708c34
louieiply <louieiply@gmail.com>
2022-06-26 21:43:18 +0930

    fix login and signup

--- public/js/login.js
+++ public/js/login.js
@@ -25,38 +25,8 @@ const loginFormHandler = async (event) => {
     }
 }
 
-const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const name = document.querySelector('#name-signup').value;
-    const email = document.querySelector('#email-signup').value;
-    const password = document.querySelector('#password-signup');
-  
-    if (name && email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ 
-            name, 
-            email, 
-            password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert(response.statusText);
-        alert('Sign-up has failed ');
-      }
-    }
-  };
-  
   document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler)
 
-
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
 
